94ef094c5b48da2f442f049d343b690742e1c48f
martin (darkmtbg2) <martin-baage@hotmail.com>
2021-04-20 12:10:37 +0200

    Services which uses the database

---
+++ StoringPasswordsH4/Service/FileService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using StoringPasswordsH4.DTO;
+
+namespace StoringPasswordsH4.Service
+{
+
+    class FileService
+    {
+
+        public static MysqlConfigurationObject GetDatabaseConfig()
+        {
+            string workingDirectory = Directory.GetCurrentDirectory();
+            //TODO: Check if the file actually exists.... (Can throw exception)
+            return JsonConvert.DeserializeObject<MysqlConfigurationObject>(File.ReadAllText(workingDirectory + @"\config.json"));
+
+        }
+    }
+}
