4a3bc8120ed3195cccbcdc3c5bca9d3982e8fee1
martin (darkmtbg2) <martin-baage@hotmail.com>
2021-04-20 09:10:20 +0200

    firstCommitWithFirstStartCode

---
+++ StoringPasswordsH4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoringPasswordsH4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
