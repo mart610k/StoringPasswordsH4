00f8da7da49f4432859a4a2d8b2c9f1e0012c394
martin (darkmtbg2) <martin-baage@hotmail.com>
2021-04-20 12:10:12 +0200

    Database related classes and script

---
+++ StoringPasswordsH4/DTO/UserPasswordObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoringPasswordsH4.DTO
+{
+    class UserPasswordObject
+    {
+        public byte[] HashedPassword { get; private set; }
+        public byte[] Salt { get; private set; }
+
+        public UserPasswordObject(byte[] hashedPassword, byte[] salt)
+        {
+            HashedPassword = hashedPassword;
+
+            Salt = salt;
+        }
+
+    }
+}
